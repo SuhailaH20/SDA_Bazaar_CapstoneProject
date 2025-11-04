973e133e60ce670bed53b941bc8d07b35de5680d
Rajja <abrajja4@gmail.com>
2025-11-04 21:40:18 +0300

    FirstDashbordpage

--- src/test/java/com/bazaarstores/pages/DashboardPage.java
+++ src/test/java/com/bazaarstores/pages/DashboardPage.java
@@ -15,6 +15,9 @@ public class DashboardPage extends BasePage {
     private final By productsLink = By.cssSelector("a[href*='products'], button:contains('Products')");
     private final By logoutButton = By.cssSelector("button:contains('Logout'), a:contains('Logout')");
     private final By userName = By.cssSelector(".user-name, [class*='username']");
+    private final By ProfileAdmin = By.xpath("//div[@class=\"avatar avatar-lg\"]");
+    private final By menu = By.xpath("//div[@class=\"avatar avatar-lg\"]");
+    private final By storemenuOption = By.xpath("//a[.//span[text()='Store']]");
 
     public DashboardPage(WebDriver driver) {
         super(driver);
@@ -70,5 +73,28 @@ public class DashboardPage extends BasePage {
     public boolean isProfileVisitChartDisplayed() {
         return isDisplayed(profileVisitChart);
     }
+
+    public boolean isProfiletAdminDisplayed() {
+        return isDisplayed(ProfileAdmin);
+    }
+
+    public boolean ismenuDisplayed() {
+        return isDisplayed(menu);
+    }
+
+    public boolean isStoerOptionDisplayed() {
+        return isDisplayed(storemenuOption);
+    }
+
+    public boolean isStoerOptionEnabel() {
+        return isEnabled(storemenuOption);
+    }
+
+
+    public StoresPage clickStoreMenuOption() {
+        click(storemenuOption);
+        return new StoresPage(driver);
+    }
+
 }
 
