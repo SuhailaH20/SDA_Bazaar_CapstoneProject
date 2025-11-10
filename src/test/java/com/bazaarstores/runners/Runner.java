8982bb086a75f7c36e0997a5eb5cebff111b34b4
RahafAlamri <rhfa9024@gmail.com>
2025-11-10 14:17:23 +0300

    update5

--- src/test/java/com/bazaarstores/runners/Runner.java
+++ src/test/java/com/bazaarstores/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com.bazaarstores.stepDefinitions",
-        tags = "@AddToCart",//@NegativeRegistration
+        tags = "@ConfirmItemsInCart",//@NegativeRegistration
         dryRun = false
 )
 public class Runner {
