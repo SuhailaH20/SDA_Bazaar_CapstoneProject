acd232b6c97627fa5dd02b52309d363bc3a93eaf
suhaila hawsawi <suhila0987@outlook.com>
2025-11-10 15:22:39 +0300

    Repush my code

--- src/test/java/com/bazaarstores/runners/Runner.java
+++ src/test/java/com/bazaarstores/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com.bazaarstores.stepDefinitions",
-        tags = "@2",//@NegativeRegistration
+        tags = "@AdminPositiveLogout",//@NegativeRegistration
         dryRun = false
 )
 public class Runner {
